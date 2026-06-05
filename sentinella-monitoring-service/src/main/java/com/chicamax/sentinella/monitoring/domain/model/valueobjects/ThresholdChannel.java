07b82f46a4e8168017ad06ca86cafe3fbdab5572
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2026-06-05 15:38:36 -0500

    feat(monitoring): value objects y servicios dominio (1/2)

---
+++ sentinella-monitoring-service/src/main/java/com/chicamax/sentinella/monitoring/domain/model/valueobjects/ThresholdChannel.java
@@ -0,0 +1,7 @@
+package com.chicamax.sentinella.monitoring.domain.model.valueobjects;
+
+public enum ThresholdChannel {
+    APP,
+    SMS,
+    EMAIL
+}
---
+++ sentinella-monitoring-service/src/main/java/com/chicamax/sentinella/monitoring/domain/model/valueobjects/ThresholdRuleOperator.java
@@ -0,0 +1,8 @@
+package com.chicamax.sentinella.monitoring.domain.model.valueobjects;
+
+public enum ThresholdRuleOperator {
+    GT,
+    LT,
+    GTE,
+    LTE
+}
