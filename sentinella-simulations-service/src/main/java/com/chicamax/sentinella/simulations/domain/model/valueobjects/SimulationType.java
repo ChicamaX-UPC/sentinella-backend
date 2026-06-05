3f5d85b269bcb4437bcc1242385446e118da82c3
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2026-06-05 15:40:07 -0500

    feat(simulations): value objects y servicios dominio (1/3)

---
+++ sentinella-simulations-service/src/main/java/com/chicamax/sentinella/simulations/domain/model/valueobjects/SimulationType.java
@@ -0,0 +1,9 @@
+package com.chicamax.sentinella.simulations.domain.model.valueobjects;
+
+public enum SimulationType {
+    WATER_LEVEL_RISE,
+    HEAVY_RAIN,
+    DIKE_SATURATION,
+    SAFETY_FACTOR,
+    SEEPAGE_DETECTION
+}
