04413dc6f369d8ffac3769761493360f444b68f4
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2026-06-05 15:40:31 -0500

    feat(profiles): API REST (3/4)

---
+++ sentinella-profiles-service/src/main/java/com/chicamax/sentinella/profiles/interfaces/rest/resources/ProfileResource.java
@@ -0,0 +1,16 @@
+package com.chicamax.sentinella.profiles.interfaces.rest.resources;
+
+import java.util.UUID;
+
+public record ProfileResource(
+        UUID userId,
+        String email,
+        String fullName,
+        String phone,
+        String jobTitle,
+        String planType,
+        Integer sensorLimit,
+        UUID subscriptionId,
+        String preferences
+) {
+}
