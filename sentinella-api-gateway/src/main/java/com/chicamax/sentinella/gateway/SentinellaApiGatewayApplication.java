02700f14d913c0efde59d5d6c05b375dc9d10518
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2026-06-05 15:38:17 -0500

    feat(gateway): aplicacion spring boot

---
+++ sentinella-api-gateway/src/main/java/com/chicamax/sentinella/gateway/SentinellaApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.chicamax.sentinella.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinellaApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinellaApiGatewayApplication.class, args);
+    }
+}
